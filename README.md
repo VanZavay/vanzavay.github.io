87696b2184974e596a9d58ea2e28299be3533cf3
VanZavay <110384899+VanZavay@users.noreply.github.com>
2022-08-01 13:38:00 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# vanzavay.github.io
---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+var myImage = document.querySelector('img');
+myImage.onclick = function(){
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/hanma.png'){
+        myImage.setAttribute ('src','images/hanma2.png');
+    }else{
+        myImage.setAttribute ('src','images/hanma3.png');
+    
+    }
+    if(mySrc === 'images/hanma3.png'){
+        myImage.setAttribute('src','images/hanma.png' );
+    }
+}
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector ('h1');
+function setUserName(){
+    var myName = prompt('Кто ты воин?');
+    localStorage.setItem('name',myName);
+    myHeading.textContent = 'Приветствую тебя,'+ myName + '!';
+    if(!localStorage.getItem('name')){
+        setUserName();
+    }else{
+        var storedName = localStoragr.getItem('name');
+        myHeading.textContent = 'Ну здравствуй, ' + storedName;
+    }
+}
+myButton.onclick = function(){
+    setUserName();
+}
